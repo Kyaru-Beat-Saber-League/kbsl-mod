7117862b16281765e0d76cfa518bfe5d0b895c7e
ribeKim <ribe@ribe.io>
2023-03-19 10:50:53 +0900

    Initialize ScoreInstaller

---
+++ Installers/ScoreInstaller.cs
@@ -0,0 +1,20 @@
+﻿using KBSL_MOD.ConfigModels;
+using UnityEngine;
+using Zenject;
+
+namespace KBSL_MOD.Installers
+{
+    public class ScoreInstaller : MonoInstaller
+    {
+        [Inject] private readonly PlayerDataModel DataModel;
+
+        public override void InstallBindings()
+        {
+            MainConfigModel mainConfig = Plugin.MainConfig;
+
+            if (!mainConfig.Enabled) return;
+
+            Plugin.Log.Notice("Loading ScoreInstaller...");
+        }
+    }
+}
