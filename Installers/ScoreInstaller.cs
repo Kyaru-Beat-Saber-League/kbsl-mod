72e5ef26c3cc981c47ce4bf3c19cd2ae01ebe217
ribeKim <ribe@ribe.io>
2023-03-21 23:37:40 +0900

    playerManager를 주입받아서 값이 나오는지 테스트

--- Installers/ScoreInstaller.cs
+++ Installers/ScoreInstaller.cs
@@ -1,11 +1,13 @@
-﻿using KBSL_MOD.ConfigModels;
-using KBSL_MOD.Events;
+﻿using KBSL_MOD.Events;
+using KBSL_MOD.Manager;
 using Zenject;
 
 namespace KBSL_MOD.Installers
 {
     public class ScoreInstaller : MonoInstaller
     {
+        [Inject] public readonly PlayerManager _playerManager;
+
         public override void InstallBindings()
         {
             var mainConfig = Plugin.MainConfig;
@@ -13,6 +15,8 @@ namespace KBSL_MOD.Installers
             if (!mainConfig.Enabled) return;
 
             Plugin.Log.Notice("Loading ScoreInstaller...");
+            Plugin.Log.Notice(_playerManager.UserName);
+            Plugin.Log.Notice(_playerManager.PlatformUserId);
 
             Container.BindInterfacesTo<NoteEventHandler>().AsSingle().NonLazy();
             Container.BindInterfacesTo<ScoreEventHandler>().AsSingle().NonLazy();
