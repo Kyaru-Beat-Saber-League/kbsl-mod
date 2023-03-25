4f53eca414b6efb6c3a2bdd8d3384a775b02f8f1
ribeKim <ribe@ribe.io>
2023-03-25 20:15:32 +0900

    Harmony로 플레이 버튼 가져오기

---
+++ Patchers/PlayButtonPatchers.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using KBSL_MOD.Utils;
+
+namespace KBSL_MOD.Patchers
+{
+    [HarmonyPatch(typeof(SinglePlayerLevelSelectionFlowCoordinator), "ActionButtonWasPressed")]
+    public static class ActionButtonListener
+    {
+        [HarmonyPrefix]
+        private static void Prefix() => GamePlayUtils.OnActionButtonWasPressed();
+
+    }
+    
+    // [HarmonyPatch(typeof(PracticeViewController), "PlayButtonPressed")]
+    // public static class PressPlayButtonListener
+    // {
+    //     [HarmonyPrefix]
+    //     private static void Prefix() => GamePlayUtils.OnActionButtonWasPressed();
+    // }
+}
