e520b5ecad988ff177d438136ff3cbb70a30fb73
ribeKim <ribe@ribe.io>
2023-03-25 20:05:59 +0900

    상태 관리를 위한 Util

---
+++ Utils/GamePlayUtils.cs
@@ -0,0 +1,20 @@
+﻿namespace KBSL_MOD.Utils
+{
+    internal static class GamePlayUtils
+    {
+        private static bool _isGameStart;
+
+        internal static bool isGameStart => _isGameStart;
+
+        internal static void Init()
+        {
+            _isGameStart = false;
+        }
+
+        internal static void OnActionButtonWasPressed()
+        {
+            _isGameStart = true;
+            Plugin.Log.Notice($"is Game Started! {isGameStart}");
+        }
+    }
+}
