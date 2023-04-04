814f850593a98fe36f2f73090822ef202081cd52
ribeKim <ribe@ribe.io>
2023-04-05 00:49:38 +0900

    WebRequest Wrapper Init

---
+++ Utils/WebRequestUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+namespace KBSL_MOD.Utils
+{
+    public static class WebRequestUtils
+    {
+        public static IEnumerator Get(string url, Action<DownloadHandler> onSuccess, Action<string> onFailure)
+        {
+            var request = UnityWebRequest.Get(url);
+            yield return request.SendWebRequest();
+
+            if (request.responseCode == 200)
+            {
+                onSuccess(request.downloadHandler);
+            }
+            else
+            {
+                onFailure(request.error);
+            }
+        }
+
+        public static IEnumerator Post(string url, List<IMultipartFormSection> form,
+            Action<DownloadHandler> onSuccess, Action onFailure)
+        {
+            var request = UnityWebRequest.Post(url, form);
+            yield return request.SendWebRequest();
+
+            if (request.responseCode == 200)
+            {
+                onSuccess(request.downloadHandler);
+            }
+            else
+            {
+                onFailure();
+            }
+        }
+    }
+}
