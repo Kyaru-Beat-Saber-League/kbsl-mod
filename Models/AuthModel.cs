1776cfa238791bfa10e68534778693dd508c5990
ribeKim <ribe@ribe.io>
2023-04-07 23:18:33 +0900

    kbsl 로그인 시 반환 객체

---
+++ Models/AuthModel.cs
@@ -0,0 +1,13 @@
+﻿namespace KBSL_MOD.Models
+{
+    public class AuthModel
+    {
+        public string AccessToken { get; set; }
+        public string ERole { get; set; }
+        public string ImageUrl { get; set; }
+        public string RefreshToken { get; set; }
+        public string TokenType { get; set; }
+        public string UserName { get; set; }
+        public int UserSeq { get; set; }
+    }
+}
