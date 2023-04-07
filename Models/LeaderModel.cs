6cc2bb00d8c87934b3065e9cc36296ff3288e3f9
ribeKim <ribe@ribe.io>
2023-04-07 23:18:44 +0900

    score을 던져줄때 필요한 객체

---
+++ Models/LeaderModel.cs
@@ -0,0 +1,20 @@
+﻿namespace KBSL_MOD.Models
+{
+    public class LeaderModel
+    {
+        public int AccLeft { get; set; }
+        public int AccRight { get; set; }
+        public double Accuracy { get; set; }
+        public int BadCut { get; set; }
+        public int BaseScore { get; set; }
+        public int BombCut { get; set; }
+        public int MissedNote { get; set; }
+        public int ModifiedScore { get; set; }
+        public int Pause { get; set; }
+        public int PlayCount { get; set; }
+        public string SongDifficulty { get; set; }
+        public string SongHash { get; set; }
+        public string SongModeType { get; set; }
+        public int WallsHit { get; set; }
+    }
+}
