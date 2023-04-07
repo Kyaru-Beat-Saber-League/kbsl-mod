10a9972eba89148b28ad9213cd387d50e59d52cd
ribeKim <ribe@ribe.io>
2023-04-07 23:19:01 +0900

    Response 반환 타입이 이렇게 생겼음

---
+++ Models/WebResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace KBSL_MOD.Models
+{
+    public class WebResponseModel<T>
+    {
+        public bool Success { get; set; }
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
