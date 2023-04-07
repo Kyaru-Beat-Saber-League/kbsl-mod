94e280d378c2fbcce4c918b77f1fa2633cad52c2
ribeKim <ribe@ribe.io>
2023-04-07 23:20:18 +0900

    Coroutine 할 때 필요한 Util

---
+++ Utils/CoroutineUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace KBSL_MOD.Utils
+{
+    public class CoroutineUtils : PersistentSingleton<CoroutineUtils>
+    {
+        public new void StartCoroutine(IEnumerator coroutine)
+        {
+            gameObject.SetActive(true);
+            base.StartCoroutine(coroutine);
+        }
+
+        public void StartCoroutine(IEnumerator coroutine, Action onFinished)
+        {
+            gameObject.SetActive(true);
+            base.StartCoroutine(CallbackCoroutine(coroutine, onFinished));
+        }
+
+        private IEnumerator CallbackCoroutine(IEnumerator coroutine, Action callback)
+        {
+            yield return coroutine;
+            callback?.Invoke();
+        }
+    }
+}
