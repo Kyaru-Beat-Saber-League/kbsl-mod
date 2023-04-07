1d593ae7351c30aa3e71afd1aabf6835c18f6a50
ribeKim <ribe@ribe.io>
2023-04-07 23:16:00 +0900

    BaseURL 설정함

--- Common/KBSLConsts.cs
+++ Common/KBSLConsts.cs
@@ -2,6 +2,7 @@
 {
     public static class KbslConsts
     {
+        public const string BaseURL = "https://api.kbsl.dev";
         public const string Auth = "https://api.kbsl.dev/auth/steam?ticket={0}";
     }
 }
